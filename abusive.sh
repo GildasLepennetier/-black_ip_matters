9c777ad7bb2ed23595d26e55c6edb48ab5f47129
Gildas Lepennetier <gildas.lepennetier@hotmail.fr>
2020-07-09 10:35:46 +0200

    update core logic

---
+++ abusive.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -e
+# check the website
+DATABASE="blacklist.abusive.txt"
+WEBSITE="https://www.abuseipdb.com/"
+NAME_DISPLAY="[abusive ip]"
+DATE=$(date +"%Y-%d-%m %T" )
+COUNT1=$(cat ${DATABASE} | wc -l)
+wget -q $WEBSITE -O index.html 2> /dev/null
+cat index.html | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" > ${DATABASE}.new
+cat ${DATABASE} ${DATABASE}.new | sort | uniq > ${DATABASE}.tmp
+mv ${DATABASE}.tmp ${DATABASE}
+COUNT2=$(cat ${DATABASE} | wc -l)
+rm index.html
+rm ${DATABASE}.new
+EXEMPLE=$(shuf -n 1 ${DATABASE})
+echo -e "$NAME_DISPLAY\t+$(($COUNT2-$COUNT1)) IP\t[total: $COUNT2]\t$DATE\t\tex:$EXEMPLE"
--- main.sh
+++ main.sh
@@ -1,17 +1,10 @@
 #!/usr/bin/env bash
 set -e
 SLEEP_MIN=1
-while [ 0 ]; do
-	DATE=$(date +"%Y-%d-%m %T" )
-	COUNT1=$(cat blacklist.txt | wc -l)
-	wget -q https://www.abuseipdb.com/ -O index.html 2> /dev/null
-	cat index.html | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" > new.txt
-	cat blacklist.txt new.txt | sort | uniq > blacklist.txt.tmp
-	mv blacklist.txt.tmp blacklist.txt
-	COUNT2=$(cat blacklist.txt | wc -l)
-	rm index.html
-	rm new.txt
-	EXEMPLE=$(shuf -n 1 blacklist.txt)
-	echo -e "+$(($COUNT2-$COUNT1)) IP\t[total: $COUNT2]\t$DATE\t\tex:$EXEMPLE"
+LIST_TODO=todo.txt
+while [ 0 ];do
+	for TODO in $(cat $LIST_TODO);do
+		bash $TODO
+	done
 	sleep $((60*$SLEEP_MIN))s
 done
