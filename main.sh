baf57b92c32cd315ddd7dd426ebb5e01f53b7f12
Gildas Lepennetier <gildas.lepennetier@hotmail.fr>
2020-07-09 10:03:15 +0200

    updated display

--- main.sh
+++ main.sh
@@ -2,15 +2,16 @@
 set -e
 SLEEP_MIN=1
 while [ 0 ]; do
-	echo -e "\nStill running $(date)"
-	echo "$(wc -l blacklist.txt) IPs before"
+	DATE=$(date +"%Y-%d-%m %T" )
+	COUNT1=$(cat blacklist.txt | wc -l)
 	wget -q https://www.abuseipdb.com/ -O index.html 2> /dev/null
 	cat index.html | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" > new.txt
 	cat blacklist.txt new.txt | sort | uniq > blacklist.txt.tmp
 	mv blacklist.txt.tmp blacklist.txt
-	echo "$(wc -l blacklist.txt) IPs total now"
+	COUNT2=$(cat blacklist.txt | wc -l)
 	rm index.html
 	rm new.txt
-	echo "exemple: $(shuf -n 1 blacklist.txt)"
+	EXEMPLE=$(shuf -n 1 blacklist.txt)
+	echo -e "+$(($COUNT2-$COUNT1)) IP\t[total: $COUNT2]\t$DATE"
 	sleep $((60*$SLEEP_MIN))s
 done
