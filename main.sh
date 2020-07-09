740e791ed9451d79d3518e0c8c538ca1e8fea5a3
Gildas Lepennetier <gildas.lepennetier@hotmail.fr>
2020-07-09 09:55:45 +0200

    fixed info new ip

--- main.sh
+++ main.sh
@@ -7,8 +7,8 @@ while [ 0 ]; do
 	wget -q https://www.abuseipdb.com/ -O index.html 2> /dev/null
 	cat index.html | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" > new.txt
 	cat blacklist.txt new.txt | sort | uniq > blacklist.txt.tmp
-	echo "$(wc -l blacklist.txt) IPs total now"
 	mv blacklist.txt.tmp blacklist.txt
+	echo "$(wc -l blacklist.txt) IPs total now"
 	rm index.html
 	rm new.txt
 	echo "exemple: $(shuf -n 1 blacklist.txt)"
